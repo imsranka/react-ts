51a7d832b4f85d10e8155612898429135946f0d3
imsranka <ranka.sakshi9@gmail.com>
2022-02-06 17:23:30 +0530

    props & optional props example

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+interface AppProps {
+  headerText: string;
+  extraText?: string;
+}
+
+function App({ headerText, extraText }: AppProps) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{headerText}</h1>
+        {extraText && <p>{extraText}</p>}
       </header>
     </div>
   );
